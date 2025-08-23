53f4b8c8b3663b1e6914a94c6bab4e7103b75433
mario <youssefbelabed10@gmail.com>
2025-08-23 01:18:05 +0100

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "MTQwNTU0MzA4MDg5NTA1Mzk1NQ.GZf3Y5.ebM8OJqNf_v0lGMcxh8TMh8XYBv1C3L1ym-47I",
+    ticket_channel: "1404985965176426536",
+    ticket_category: "1404985816308252742",
+    ticket_logs: "1404987967415652462",
+    support_team: "1405586764755898581",
+};
